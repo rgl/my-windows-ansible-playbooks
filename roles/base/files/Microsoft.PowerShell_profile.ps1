6f7944ce19a3ccc444ed114104a34ffabefed708
Rui Lopes <rgl@ruilopes.com>
2022-06-10 11:59:09 +0100

    install oh-my-posh

--- roles/base/files/Microsoft.PowerShell_profile.ps1
+++ roles/base/files/Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,10 @@
 Set-PSReadLineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Configure oh-my-posh.
+if ($PSVersionTable.PSEdition -eq 'Desktop') {
+    oh-my-posh init powershell --config ~/.rgl.omp.json | Invoke-Expression
+} else {
+    oh-my-posh init pwsh --config ~/.rgl.omp.json | Invoke-Expression
+}
