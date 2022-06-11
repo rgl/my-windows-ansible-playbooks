6752d6a90cc37d4a9ec7695f8ce128bdd6115892
Rui Lopes <rgl@ruilopes.com>
2022-06-11 07:40:51 +0100

    configure visual studio

---
+++ roles/visual-studio/files/settings.ps1
@@ -0,0 +1,40 @@
+# set the visual studio settings.
+# see https://docs.microsoft.com/en-us/visualstudio/ide/reference/resetsettings-devenv-exe?view=vs-2022
+# see https://docs.microsoft.com/en-us/visualstudio/ide/how-to-change-fonts-and-colors-in-visual-studio?view=vs-2022
+# see https://github.com/rgl/visual-studio-community-vagrant/blob/master/provision-vs.ps1#L55-L70
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
+
+$vsHome = 'C:\Program Files\Microsoft Visual Studio\2022\Community'
+$devenv = "$vsHome\Common7\IDE\devenv.com"
+$settingsHomePath = "$env:LOCALAPPDATA\Microsoft\VisualStudio"
+$changed = $false
+
+# set the visual studio settings.
+# NB this is required to initialize the settings before Visual Studio actually
+#    starts for the first time.
+# e.g. C:\Users\Administrator\AppData\Local\Microsoft\VisualStudio\17.0_4265cb20\Settings\CurrentSettings.vssettings
+$currentSettingsPath = if (Test-Path $settingsHomePath) {
+    Get-ChildItem -Recurse $settingsHomePath -Include CurrentSettings.vssettings
+} else {
+    $null
+}
+if (!$currentSettingsPath) {
+    Write-Host 'Resetting Settings to Default...'
+    if (Test-Path $settingsHomePath) {
+        Remove-Item -Recurse -Force $settingsHomePath
+    }
+    &$devenv /NoSplash /ResetSettings General /Command Exit | Out-String -Stream
+    Write-Host 'Modifying the Default Settings...'
+    $currentSettingsPath = Get-ChildItem -Recurse "$settingsHomePath\CurrentSettings.vssettings"
+    $defaultSettingsPath = "$(Split-Path -Parent $currentSettingsPath)\DefaultSettings.vssettings"
+    Move-Item $currentSettingsPath $defaultSettingsPath
+    $xsl = New-Object System.Xml.Xsl.XslCompiledTransform
+    $xsl.Load("$env:USERPROFILE\.ansible-visual-studio\settings.xsl")
+    $xsl.Transform($defaultSettingsPath, $currentSettingsPath)
+    $changed = $true
+}
+
+$Ansible.Changed = $changed
