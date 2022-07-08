64ba640112d0ed1dcb0926c2f81d9aa514aadc0c
Rui Lopes <rgl@ruilopes.com>
2022-07-08 08:05:22 +0100

    show information about the ansible execution environment

--- ansible.sh
+++ ansible.sh
@@ -7,6 +7,16 @@ tag="ansible-$(basename "$PWD")"
 # build the ansible image.
 DOCKER_BUILDKIT=1 docker build -f Dockerfile.ansible -t "$tag" .
 
+# show information about the execution environment.
+docker run --rm -i "$tag" bash <<'EOF'
+exec 2>&1
+set -euxo pipefail
+cat /etc/os-release
+ansible --version
+python3 -m pip list
+ansible-galaxy collection list
+EOF
+
 # execute command (e.g. ansible-playbook).
 # NB the GITHUB_ prefixed environment variables are used to trigger ansible-lint
 #    to annotate the GitHub Actions Workflow with the linting violations.
