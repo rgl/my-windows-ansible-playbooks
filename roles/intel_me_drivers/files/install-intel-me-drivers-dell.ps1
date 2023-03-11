22c5e271d9747260e3d30ee9411a5bab49581d74
Rui Lopes <rgl@ruilopes.com>
2023-03-11 14:40:26 +0000

    add direct link to the dell-drivers-scraper data optiplex-7060-desktop.json file

--- roles/intel_me_drivers/files/install-intel-me-drivers-dell.ps1
+++ roles/intel_me_drivers/files/install-intel-me-drivers-dell.ps1
@@ -1,5 +1,5 @@
 # see https://www.dell.com/support/home/en-us/product-support/product/optiplex-7060-desktop/drivers
-# see https://github.com/rgl/dell-drivers-scraper
+# see https://github.com/rgl/dell-drivers-scraper/blob/main/data/optiplex-7060-desktop.json
 # NB this installs the binaries at C:\Program Files (x86)\Intel\Intel(R) Management Engine Components.
 # NB this also installs an uninstaller named Intel(R) Management Engine Components.
 
