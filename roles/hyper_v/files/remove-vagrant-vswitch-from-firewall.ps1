893cee3a17ca589b58c146a98e72beef6d9d4149
Rui Lopes <rgl@ruilopes.com>
2023-03-25 15:51:09 +0000

    correctly set the ansible changed variable value

--- roles/hyper_v/files/remove-vagrant-vswitch-from-firewall.ps1
+++ roles/hyper_v/files/remove-vagrant-vswitch-from-firewall.ps1
@@ -4,15 +4,12 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
-$changed = $false
 $name = 'Vagrant'
 $netAdapterName = "vEthernet ($name)"
 
 Get-NetFirewallProfile | ForEach-Object {
     if ($_.DisabledInterfaceAliases -notcontains $netAdapterName) {
         Set-NetFirewallProfile -DisabledInterfaceAliases @($_.DisabledInterfaceAliases, $netAdapterName)
-        $changed = $true
+        $Ansible.Changed = $true
     }
 }
-
-$Ansible.Changed = $changed
