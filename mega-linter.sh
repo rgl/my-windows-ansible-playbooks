40f37f99f0a66f67994b332041c9a412db674cad
Rui Lopes <rgl@ruilopes.com>
2023-08-20 16:42:36 +0100

    upgrade to megalinter 7.3.0

--- mega-linter.sh
+++ mega-linter.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 image_name='oxsecurity/megalinter'
 # see https://hub.docker.com/r/oxsecurity/megalinter/tags
 # renovate: datasource=docker depName=oxsecurity/megalinter
-image_version='7.2.1'
+image_version='7.3.0'
 
 # see https://megalinter.io/latest/configuration/
 # see https://megalinter.io/latest/descriptors/powershell/
