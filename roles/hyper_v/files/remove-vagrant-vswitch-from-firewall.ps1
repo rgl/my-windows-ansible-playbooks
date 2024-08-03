dd19e368a5a5889caf7acf260da4550e52b2df26
Rui Lopes <rgl@ruilopes.com>
2024-08-03 20:47:50 +0100

    ensure that only valid network interfaces aliases are used

--- roles/hyper_v/files/remove-vagrant-vswitch-from-firewall.ps1
+++ roles/hyper_v/files/remove-vagrant-vswitch-from-firewall.ps1
@@ -6,10 +6,17 @@ $ProgressPreference = 'SilentlyContinue'
 
 $name = 'Vagrant'
 $netAdapterName = "vEthernet ($name)"
+$validNetAdapterNames = (Get-NetAdapter).Name
 
 Get-NetFirewallProfile | ForEach-Object {
     if ($_.DisabledInterfaceAliases -notcontains $netAdapterName) {
-        Set-NetFirewallProfile -DisabledInterfaceAliases @($_.DisabledInterfaceAliases, $netAdapterName)
+        # ensure only valid aliases are used, otherwise, Set-NetFirewallProfile fails.
+        $disabledInterfaceAliases = @($_.DisabledInterfaceAliases | Where-Object {
+            $validNetAdapterNames -contains $_
+        })
+        Set-NetFirewallProfile `
+            -Profile $_.Name `
+            -DisabledInterfaceAliases ($disabledInterfaceAliases + $netAdapterName)
         $Ansible.Changed = $true
     }
 }
