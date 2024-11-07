69671818cd1b3ac66d1fbe4113090937e50d5278
Rui Lopes <rgl@ruilopes.com>
2024-11-07 10:29:10 +0000

    upgrade to netbird 0.31.0

--- roles/netbird/files/install-netbird.ps1
+++ roles/netbird/files/install-netbird.ps1
@@ -16,7 +16,7 @@ $binaryPath = "$env:ProgramFiles\Netbird\netbird.exe"
 
 # check whether the expected version is already installed.
 $installBinaries = if (Test-Path $binaryPath) {
-    # e.g. 0.30.3
+    # e.g. 0.31.0
     $actualVersionText = &$binaryPath version
     if ($actualVersionText -notmatch '(.+)') {
         throw "unable to parse the netbird.exe version from: $actualVersionText"
