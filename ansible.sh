8edd83b8d4a1d3859c6d0dfd8d95fa115e958566
Rui Lopes <rgl@ruilopes.com>
2025-02-06 08:45:22 +0000

    upgrade to ansible-lint 25.1.2

--- ansible.sh
+++ ansible.sh
@@ -21,7 +21,7 @@ EOF
 # execute command (e.g. ansible-playbook).
 # NB the GITHUB_ prefixed environment variables are used to trigger ansible-lint
 #    to annotate the GitHub Actions Workflow with the linting violations.
-#    see https://github.com/ansible/ansible-lint/blob/v25.1.1/src/ansiblelint/app.py#L106-L107
+#    see https://github.com/ansible/ansible-lint/blob/v25.1.2/src/ansiblelint/app.py#L106-L107
 #    see https://ansible-lint.readthedocs.io/en/latest/usage/#ci-cd
 exec docker run \
     --rm \
