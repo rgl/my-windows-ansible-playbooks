9cfe497c6c7fe6f6516847e1e88f7a8cc4dd0f10
Rui Lopes <rgl@ruilopes.com>
2025-04-18 22:34:00 +0000

    upgrade to docker compose 2.35.0

--- roles/docker/files/install-docker-compose.ps1
+++ roles/docker/files/install-docker-compose.ps1
@@ -18,7 +18,7 @@ $dockerComposePath = "$dockerCliPluginsPath\docker-compose.exe"
 
 # check whether the expected version is already installed.
 $installBinaries = if (Test-Path $dockerComposePath) {
-    # e.g. Docker Compose version v2.34.0
+    # e.g. Docker Compose version v2.35.0
     $actualVersionText = &$dockerComposePath version
     if ($actualVersionText -notmatch '^Docker Compose version v(\d+(\.\d+)+)') {
         throw "unable to parse the docker-compose.exe version from: $actualVersionText"
