2992dc19a8ecb0e15b4b87cae7f7ea11271b2bde
Rui Lopes <rgl@ruilopes.com>
2025-05-04 14:09:51 +0000

    upgrade to wsl ubuntu 24.04

--- roles/wsl/files/install-wsl-ubuntu.ps1
+++ roles/wsl/files/install-wsl-ubuntu.ps1
@@ -1,6 +1,6 @@
 # NB you can remove a distro altogether with, e.g.:
-#       wsl --unregister Ubuntu-22.04
-#       Remove-Item -Recurse C:\Wsl\Ubuntu-22.04
+#       wsl --unregister Ubuntu-24.04
+#       Remove-Item -Recurse C:\Wsl\Ubuntu-24.04
 
 param(
     [string]$distroName,
@@ -20,7 +20,19 @@ $Ansible.Changed = $false
 $env:WSL_UTF8 = '1'
 
 function wsl {
-    &"$env:ProgramFiles\WSL\wsl.exe" @Args
+    # NB we have to change the preference (and 2>&1) because wsl.exe writes to
+    #    stderr and that trips powershell into thinking the command failed...
+    #    the string that is written to stderr is:
+    #       wsl: Failed to configure network (networkingMode Nat), falling back to networkingMode VirtioProxy.
+    #    that does not seem to prevent wsl.exe from actually working, so this
+    #    is not looking for that error at all. nor any other error string,
+    #    instead, we rely on the exit code.
+    $ErrorActionPreference = 'Continue'
+    if (Test-Path variable:PSNativeCommandUseErrorActionPreference) {
+        $PSNativeCommandUseErrorActionPreference = $false
+    }
+    &"$env:ProgramFiles\WSL\wsl.exe" @Args 2>&1
+    $ErrorActionPreference = 'Stop'
     if ($LASTEXITCODE) {
         throw "failed with exit code $LASTEXITCODE"
     }
@@ -29,9 +41,22 @@ function wsl {
 function Invoke-WslDistroScript([string]$distroName, [string]$script) {
     $scriptPath = 'C:\Windows\Temp\invoke-wsl-script.sh'
     Set-Content -NoNewline -Encoding ascii -Path $scriptPath -Value $script
+    # NB we have to change the preference (and 2>&1) because wsl.exe writes to
+    #    stderr and that trips powershell into thinking the command failed...
+    #    the string that is written to stderr is:
+    #       wsl: Failed to configure network (networkingMode Nat), falling back to networkingMode VirtioProxy.
+    #    that does not seem to prevent wsl.exe from actually working, so this
+    #    is not looking for that error at all. nor any other error string,
+    #    instead, we rely on the exit code.
+    $ErrorActionPreference = 'Continue'
+    if (Test-Path variable:PSNativeCommandUseErrorActionPreference) {
+        $PSNativeCommandUseErrorActionPreference = $false
+    }
     &"$env:ProgramFiles\WSL\wsl.exe" --distribution $distroName -- `
         /mnt/c/Windows/Temp/invoke-wsl-script.sh `
-        @Args
+        @Args `
+        2>&1
+    $ErrorActionPreference = 'Stop'
     if ($LASTEXITCODE) {
         throw "failed to execute $scriptPath inside the $distroName wsl distro with exit code $LASTEXITCODE"
     }
