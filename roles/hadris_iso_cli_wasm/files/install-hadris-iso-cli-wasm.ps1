07dbc4cd037eb772601cdc86e15c5986a38f0f22
Rui Lopes <rgl@ruilopes.com>
2026-09-24 21:30:24 +0000

    upgrade to hadris-iso-cli-wasm 2.4.20260920

--- roles/hadris_iso_cli_wasm/files/install-hadris-iso-cli-wasm.ps1
+++ roles/hadris_iso_cli_wasm/files/install-hadris-iso-cli-wasm.ps1
@@ -14,7 +14,7 @@ $binaryPath = "$env:ChocolateyInstall\bin\hadris-iso-cli.wasm"
 
 # bail when its already installed.
 if (Test-Path $binaryPath) {
-    # e.g. hadris-iso 2.1.20260823
+    # e.g. hadris-iso 2.4.20260920
     $actualVersionText = wasmtime.exe $binaryPath --version
     if ($actualVersionText -notmatch '^hadris-iso (.+)') {
         throw "unable to parse the hadris-iso-cli.wasm version from: $actualVersionText"
